18ad8403da834c977420af192c03f0e6df1a148c
Piotr Ziętek <pzietek93@gmail.com>
2020-06-18 22:14:33 +0200

    Initial commit

---
+++ src/main/java/com/fb2020/Store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.fb2020.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fb2020/Store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb2020.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
