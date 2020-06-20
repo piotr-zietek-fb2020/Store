57f7d3b605dfba27ebc2e7a1a556812e4115aaf6
Piotr Ziętek <pzietek93@gmail.com>
2020-06-20 19:00:04 +0200

    new function

--- src/main/java/com/fb2020/Store/repository/OrderRepository.java
+++ src/main/java/com/fb2020/Store/repository/OrderRepository.java
@@ -4,6 +4,9 @@ import com.fb2020.Store.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByClientId(Long id);
 }
