c08705cf33525504553220bb173be71c04f1e8a0
Piotr Ziętek <pzietek93@gmail.com>
2020-06-21 00:47:53 +0200

    added OrderDetails Repository

--- src/main/java/com/fb2020/Store/repository/OrderDetailsRepository.java
+++ src/main/java/com/fb2020/Store/repository/OrderDetailsRepository.java
@@ -4,6 +4,9 @@ import com.fb2020.Store.entity.OrderDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
+    List<OrderDetails> findAllByOrderId(Long id);
 }
