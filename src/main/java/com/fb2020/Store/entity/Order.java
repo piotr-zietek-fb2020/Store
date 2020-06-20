be41538b04c48189a31f52fb29ccd06aee416758
Piotr Ziętek <pzietek93@gmail.com>
2020-06-20 13:53:38 +0200

    added OrderDetails to Order

--- src/main/java/com/fb2020/Store/entity/Order.java
+++ src/main/java/com/fb2020/Store/entity/Order.java
@@ -1,11 +1,14 @@
 package com.fb2020.Store.entity;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.math.BigDecimal;
+
+import java.sql.Date;
+import java.util.List;
 
 
 @Entity
-@Table(name = "order")
+@Table(name = "`order`")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,8 +16,44 @@ public class Order {
     @Column(name = "order_date")
     private Date orderDate;
     @Column(name = "total_cost")
-    private double totalCost;
+    private BigDecimal totalCost;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private Client client;
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetails> orderDetails;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public BigDecimal getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(BigDecimal totalCost) {
+        this.totalCost = totalCost;
+    }
+    public List<OrderDetails> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetails> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
 }
